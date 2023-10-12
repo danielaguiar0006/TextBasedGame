17d280acdd2f21e6352d883e4f9c21149efaf5f2
Daniel Aguiar <danielaguiar0006@gmail.com>
2023-10-11 23:46:31 -0700

    Complete item and mob map
    Completed maps for mob and items.
    moved things into setup function for readability

--- TextBasedGame/TextBasedGame.py
+++ TextBasedGame/TextBasedGame.py
@@ -52,13 +52,26 @@ class Item():
         self.effect = effect  # This can be a function or any other effect mechanism
 
 
-def main():
-    player = Player()
+def setup():
+    """Initialize game settings and world."""
+    print('Setup...\n')
 
+    # Map of all of our rooms in the game and their relative direction to each other
+    rooms_map = {'Entrance Hall': {'East': 'The Majestic Dining Room'},
+                 'The Majestic Dining Room': {'North': 'The Desolate Kitchen', 'East': 'The Cursed Dungeon',
+                                              'South': 'The Abandoned Bedroom', 'West': 'Entrance Hall'},
+                 'The Desolate Kitchen': {'East': 'The Haunted Chamber', 'South': 'The Majestic Dining Room'},
+                 'The Haunted Chamber': {'West': 'The Desolate Kitchen'},
+                 'The Cursed Dungeon': {'North': 'The Dragon\'s Lair', 'West': 'The Majestic Dining Room'},
+                 'The Dragon\'s Lair': {'South': 'The Cursed Dungeon'},
+                 'The Abandoned Bedroom': {'North': 'The Majestic Dining Room', 'East': 'The Forgotten Library'},
+                 'The Forgotten Library': {'East': 'The Abandoned Bedroom'}
+                 }
     # Items in the game
     torch_item = Item('Torch', 'no description')
-    health_potion = Item('Elixir of Vitality', 'health potion', consumable=True)  # ! NEEDS TO AFFECTS PLAYERS HEALTH
-    enchanted_armour = Item('Enchanted Chestplate', 'Provides more protection against attacks')
+    health_potion_item = Item('Elixir of Vitality', 'health potion',
+                              consumable=True)  # ! NEEDS TO AFFECTS PLAYERS HEALTH
+    enchanted_armour_item = Item('Enchanted Chestplate', 'Provides more protection against attacks')
     sword_item = Item('The Dragon Slayer Sword', 'Big sword which increases attack power substantially')
     key_item = Item('Rusty Key', 'Who knows what it unlocks, but surely it\'s for something special...',
                     consumable=True)
@@ -69,31 +82,40 @@ def main():
     hell_hound_1 = Mob('Hell Hound', health=75, damage=15)
     hell_hound_2 = Mob('Hell Hound', health=75, damage=15)
     hell_hound_3 = Mob('Hell Hound', health=75, damage=15)
+    dragon = Mob('Ancient Dragon', health=500, damage=25)
 
-    # Map of all of our rooms in the game and their relative direction to each other
-    rooms = {'Entrance Hall': {'East': 'The Majestic Dining Room'},
-             'The Majestic Dining Room': {'North': 'The Desolate Kitchen', 'East': 'The Cursed Dungeon',
-                                          'South': 'The Abandoned Bedroom', 'West': 'Entrance Hall'},
-             'The Desolate Kitchen': {'East': 'The Haunted Chamber', 'South': 'The Majestic Dining Room'},
-             'The Haunted Chamber': {'West': 'The Desolate Kitchen'},
-             'The Cursed Dungeon': {'North': 'The Dragon\'s Lair', 'West': 'The Majestic Dining Room'},
-             'The Dragon\'s Lair': {'South': 'The Cursed Dungeon'},
-             'The Abandoned Bedroom': {'North': 'The Majestic Dining Room', 'East': 'The Forgotten Library'},
-             'The Forgotten Library': {'East': 'The Abandoned Bedroom'}
-             }
-    room_items = {'Entrance Hall': [torch_item]}
-    room_mobs = {'Entrance Hall': []}
+    # Map of Items in all their respective rooms
+    room_items = {'Entrance Hall': [torch_item],
+                  'The Majestic Dining Room': [enchanted_armour_item],
+                  'The Desolate Kitchen': [health_potion_item],
+                  'The Haunted Chamber': [],
+                  'The Cursed Dungeon': [sword_item],
+                  'The Dragon\'s Lair': [],  # After Killing dragon it should drop 'Dragons Heart,' ending the game
+                  'The Abandoned Bedroom': [key_item],
+                  'The Forgotten Library': [health_potion_item]
+                  }
+    # Map of Mobs in all their respective rooms
+    room_mobs = {'Entrance Hall': [], 'The Majestic Dining Room': [], 'The Desolate Kitchen': [],
+                 'The Haunted Chamber': [undead_soldier_1, undead_soldier_2],
+                 'The Cursed Dungeon': [hell_hound_1, hell_hound_2, hell_hound_3],
+                 'The Dragon\'s Lair': [dragon], 'The Abandoned Bedroom': [], 'The Forgotten Library': []
+                 }
+    return rooms_map, room_items, room_mobs
+
+
+def main():
+    rooms_map, room_items, room_mobs = setup()
+    player = Player()
 
     show_instructions()
-
     while player.is_alive:
-        hud(player, room_mobs, room_items)
+        display_hud(player, room_mobs, room_items)
 
         command = input('Enter your move:').split()
         action = command[0].lower()
 
         if action == 'go':
-            player.move(command[1], rooms)
+            player.move(command[1], rooms_map)
 
 
 # Sample function showing the goal of the game and move commands
@@ -105,7 +127,7 @@ def show_instructions():  # ! NEEDS TO BE REWRITTEN TO MATCH MY GAME
     print("Add to Inventory: get 'item name'")
 
 
-def hud(player, room_mobs, room_items):
+def display_hud(player, room_mobs, room_items):
     print(f'You are in {player.location}')
     for mob in room_mobs[player.location]:
         print(f'{mob.name}, Health: {mob.health}')
@@ -114,5 +136,6 @@ def hud(player, room_mobs, room_items):
 
     print(f'Inventory : {player.inventory}')
     print('-------------------------------------')
+
 
 main()
